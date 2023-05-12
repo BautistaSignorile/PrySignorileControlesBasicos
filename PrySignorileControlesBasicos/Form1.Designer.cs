b28970ca3a62f24ef9cb1007fe380a63bb236464
Bautista <bautistasignorile@gmail.com>
2023-05-12 12:48:19 -0300

    1

--- PrySignorileControlesBasicos/Form1.Designer.cs
+++ PrySignorileControlesBasicos/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PrySignorileControlesBasicos
 {
-    partial class Form1
+    partial class frmControlesBasicos
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmControlesBasicos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmControlesBasicos";
+            this.Text = "Controles Basicos";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- PrySignorileControlesBasicos/Form1.cs
+++ PrySignorileControlesBasicos/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PrySignorileControlesBasicos
 {
-    public partial class Form1 : Form
+    public partial class frmControlesBasicos : Form
     {
-        public Form1()
+        public frmControlesBasicos()
         {
             InitializeComponent();
         }
